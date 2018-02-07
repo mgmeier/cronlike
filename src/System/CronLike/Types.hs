f1e80967d65b83f35407bd2ec552cec4bd923cf4
Michael Karg <michael@kopfarbyte.com>
2018-02-07 16:16:13 +0100

    Show instances

--- src/System/CronLike/Types.hs
+++ src/System/CronLike/Types.hs
@@ -7,11 +7,15 @@ import Data.Time.Clock      (UTCTime, NominalDiffTime)
 
 data CronLikeIntervall
     = IntervEveryNMins  Int
-    | IntervDailyAt     TimeOfDay
+    | IntervDailyAt     [TimeOfDay]
     | IntervScheduled   UTCTime (Maybe NominalDiffTime)
+    deriving Show
 
 data CronLikeJob = CronLikeJob
     { cjobName      :: String
     , cjobInterval  :: CronLikeIntervall
     , cjobAction    :: IO ()
     }
+
+instance Show CronLikeJob where
+    show c = "CronLikeJob " ++ cjobName c ++ " @ " ++ show (cjobInterval c)
