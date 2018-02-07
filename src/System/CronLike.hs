5c84e53df82cbadbc4d08725338681e59846eb6b
Michael Karg <michael@kopfarbyte.com>
2018-02-07 16:08:12 +0100

    basic machinery

--- src/System/CronLike.hs
+++ src/System/CronLike.hs
@@ -1,41 +1,90 @@
-
+{-# LANGUAGE LambdaCase #-}
 
 module System.CronLike
         ( cronlikeRegister
         , cronlikeUnregister
         , cronlikeIsRunning
+        , cronlikeList
+        , cronlikeStop
+        , cronlikeStart
         , module Types
         ) where
 
 import  System.CronLike.Types as Types
 
+import  Control.Concurrent.Async        (race)
+import  Control.Concurrent              (threadDelay, forkIO)
 import  Control.Monad
 import  Control.Concurrent.MVar
-import  Data.IORef
-import  Data.Maybe                      (fromMaybe)
+import  Data.Maybe
 import  System.IO.Unsafe                (unsafePerformIO)
 
 
+type Command = Either String CronLikeJob
 
 --- global mutables
 
--- empty -> not running; True -> running; False -> stop requested
-gmutRunningState :: MVar Bool
-gmutRunningState = unsafePerformIO newEmptyMVar
-{-# NOINLINE gmutRunningState #-}
+-- full -> stopped; empty -> running
+gmutRunning :: MVar ()
+gmutRunning = unsafePerformIO $ newMVar ()
+{-# NOINLINE gmutRunning #-}
+
+gmutJobs :: MVar [CronLikeJob]
+gmutJobs = unsafePerformIO $ newMVar []
+{-# NOINLINE gmutJobs #-}
+
+gmutControl :: MVar Command
+gmutControl = unsafePerformIO newEmptyMVar
+{-# NOINLINE gmutControl #-}
+
+
+
+
+scheduler :: IO ()
+scheduler =
+    readMVar gmutJobs >>= \case
+        [] -> event >>= control
+        js -> race (nextJob js) event >>= react
+  where
+    react = either (\j -> scheduler) control
+    event = race (takeMVar gmutRunning) (takeMVar gmutControl)
+   
+    control :: Either () Command -> IO ()
+    control Left{} = cronlikeStop
+    control (Right command) = do
+        modifyMVar_ gmutJobs (return . modify)
+        scheduler
+      where
+        modify = either (\n -> filter ((/= n) . cjobName)) (:) command
+
+    nextJob js = do 
+        -- handle filtering out scheduled events in the past
+        threadDelay (findShortestDelay js)
+        return $ head js
+    
+    findShortestDelay _ = 
+        60 * 1000 * 1000
+  
+
+
+-- API
 
 cronlikeRegister :: CronLikeJob -> IO ()
-cronlikeRegister = undefined
+cronlikeRegister = putMVar gmutControl . Right 
 
 cronlikeUnregister :: String -> IO ()
-cronlikeUnregister = undefined
+cronlikeUnregister = putMVar gmutControl . Left
+
+cronlikeList :: IO [CronLikeJob]
+cronlikeList = readMVar gmutJobs
 
 cronlikeIsRunning :: IO Bool
-cronlikeIsRunning = fromMaybe False <$> tryReadMVar gmutRunningState
+cronlikeIsRunning = isEmptyMVar gmutRunning
+
+cronlikeStop :: IO ()
+cronlikeStop = void $ tryPutMVar gmutRunning ()
 
 cronlikeStart :: IO ()
-cronlikeStart = isEmptyMVar gmutRunningState >>= (`when` start)
-  where
-    start = do
-        tryPutMVar gmutRunningState True
-        putStrLn "starting cron"
+cronlikeStart =
+    tryTakeMVar gmutRunning >>= \res ->
+        when (isJust res) (void $ forkIO scheduler)
